17ac57e2a62fdded9bb1fe788afccb7dbcf60274
TomekT <tomtrusz@gmail.com>
2014-08-12 00:43:42 +0200

    Linear regression proof of concept

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <string>
 #include <map>
+#include <cmath>
 
 struct node {
 public:
@@ -27,13 +28,12 @@ class dummy_data_source :
     public data_source
 {
 public:
-
-    virtual std::reference_wrapper<node> get(node::node_id id) {
-        ;
-    }
     virtual std::reference_wrapper<node> get(const std::string& name) {
         auto ret = nodes.insert(std::make_pair(name, node(name)));
         return std::ref((*ret.first).second);
+    }
+    virtual std::reference_wrapper<node> get(node::node_id id) { 
+
     }
     virtual void  debug_print_nodes() {
         for (auto& node : nodes) {
@@ -53,6 +53,63 @@ std::vector< std::pair<std::string, std::string> > desc {
     {"file_b.txt", "text"}
 };
 
+class linear_regression {
+public:
+    linear_regression(size_t num_features) :
+        params(num_features + 1, 0.0f), // +1 due to the x0 feature
+        alpha(0.05f)
+    {}
+
+    inline float h_fun(const std::vector<float>& in_params,
+               const std::vector<float>& in_x)
+    {
+        float h_val = in_params[0];
+        for (size_t i = 0; i < in_x.size(); ++i) {
+            h_val += in_params[i + 1] * in_x[i];
+        }
+        return h_val;
+    }
+
+    void train(
+        const std::vector< std::vector<float> >& vx,
+        const std::vector< float >& vy)
+    {
+        float divergence = 1000.0f;
+        // Gradient descent
+        while (divergence != 0.0f) {
+            auto cp_params = params;
+            float inner = 0;
+            float trail_x = 1.0f;
+            divergence = 0;
+
+            for (size_t i = 0; i < vx.size(); ++i) {
+                float h_val = h_fun(cp_params, vx[i]);
+                inner += (h_val - vy[i]) * trail_x;
+            }
+            float outer = cp_params[0] - (alpha * (1.0f/vx.size()) * inner);
+            divergence += fabs(params[0] - outer);
+            params[0] = outer;
+
+            // Params greater than 0;
+            for (size_t p = 1; p < cp_params.size(); ++p) {
+                inner = 0;
+                for (size_t i = 0; i < vx.size(); ++i) {
+                    float h_val = h_fun(cp_params, vx[i]);
+                    inner += (h_val - vy[i]) * vx[i][p - 1];
+                }
+                outer = cp_params[p] - (alpha * (1.0f/vx.size()) * inner);
+                divergence += fabs(params[p] - outer);
+                params[p] = outer;
+            }
+        }
+        for (auto param : params) { std::cout << param << " "; }
+        std::cout << std::endl;
+    }
+private:
+    std::vector<float> params;
+    float alpha;
+};
+
 int main(int argc, char **argv)
 {
     data_source *ts = new dummy_data_source;
@@ -65,6 +122,16 @@ int main(int argc, char **argv)
     }
 
     ts->debug_print_nodes();
+    delete ts;
+
+    linear_regression lr(1);
+    std::vector< std::vector<float> > x {
+        {1}, {2}, {3}, {4}, {5}
+    };
+    std::vector<float> y { 2.0f, 4.0f, 5.0f, 4.0f, 5.0f };
+    lr.train(x, y);
+
+
 
     return 0;
 };
