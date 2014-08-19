6c0c6bf7d709cd8db00b3ab463289a73a9bb2a4a
TomekT <tomtrusz@gmail.com>
2014-08-19 22:54:11 +0200

    Major refactoring + loading from file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 // STD lib
 #include <iostream>
+#include <memory>
 #include <fstream>
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <sstream>
 #include <map>
 #include <cmath>
 
@@ -16,34 +18,48 @@ public:
     node(const std::string& in_name) : id(0), name(in_name) {}
     node_id id;
     std::string name;
-    std::vector< std::reference_wrapper<node> > similiar;
+    std::string system_path;
+    std::vector<node*> similiar;
 };
 
-class tag_engine
+class file_graph
 {
 public:
-    std::reference_wrapper<node> get(const std::string& name) {
-        auto ret = nodes.insert(std::make_pair(name, node(name)));
-        return std::ref((*ret.first).second);
+    ~file_graph() {
+        for (auto& node : nodes) {
+            delete node.second;
+        }
     }
-    std::reference_wrapper<node> get(node::node_id id) { 
+    node *get(const std::string& name) {
+        auto result = nodes.equal_range(name);
+        if (result.first == result.second) {
+            std::cout << "C++ magic!" << std::endl;
+            return nodes.insert(result.second, std::make_pair(name, new node(name)))->second;
+        }
+        std::cout << "C++ standard equal_range worked!" << std::endl;
+        return result.first->second;
+    }
+//    node *get_file(const std::string& name) {
+//
+//    }
+    node *get(node::node_id id) { 
         throw std::runtime_error("Not implemented\n");
     }
-    void add_link(std::reference_wrapper<node> first, std::reference_wrapper<node> second) {
-        first.get().similiar.push_back(second);
-        second.get().similiar.push_back(first);
+    void add_link(node *first, node *second) {
+        first->similiar.push_back(second);
+        second->similiar.push_back(first);
     }
     void  debug_print_nodes() {
         for (auto& node : nodes) {
-            std::cout << node.second.name << "\n";
-            for (auto& doc : node.second.similiar) {
-                std::cout << doc.get().name << " ";
+            std::cout << node.second->name << "\n";
+            for (auto& doc : node.second->similiar) {
+                std::cout << doc->name << " ";
             }
             std::cout << std::endl;
         }
     }
 private:
-    std::map<std::string, node> nodes;
+    std::map<std::string, node*> nodes;
 };
 
 std::vector< std::pair<std::string, std::string> > desc {
@@ -52,24 +68,42 @@ std::vector< std::pair<std::string, std::string> > desc {
 };
 
 class data_source {
-    virtual node fetch_item(const std::string& name) = 0;
-    virtual std::vector<node> fetch_dir(const std::string& name) = 0;
+public:
+    virtual std::unique_ptr<file_graph> fetch_dir(const std::string& path) = 0;
+};
+
+class config_data_source :
+    public data_source
+{
+public:
+    virtual std::unique_ptr<file_graph> fetch_dir(const std::string& path) {
+        std::string cfg_file = path + ((path.back() != '/') ? ".tagrc" : "/.tagrc");
+        std::ifstream cfg_ifs(cfg_file, std::ifstream::in);
+        
+        std::unique_ptr<file_graph> graph(new file_graph);
+
+        std::string line, sys_file_path, tag_name;
+        while (std::getline(cfg_ifs, line)) {
+            std::istringstream iss(line);
+            std::getline(iss, sys_file_path, ',');
+            std::getline(iss, tag_name, ',');
+
+            std::cout << sys_file_path << " / " << tag_name << std::endl;
+            auto file_node = graph->get(sys_file_path);
+            auto tag_node = graph->get(tag_name);
+            graph->add_link(file_node, tag_node);
+        }
+
+        return graph;
+    }
 };
 
 int main(int argc, char **argv)
 {
-    tag_engine *ts = new tag_engine;
-
-    for (auto& p : desc)
-    {
-        std::cout << p.first << " " << p.second << std::endl;
-        auto file_node = ts->get(p.first);
-        auto tag_node = ts->get(p.second);
-        ts->add_link(file_node, tag_node);
-    }
+    config_data_source cds;
+    std::unique_ptr<file_graph> ts = cds.fetch_dir("./");
 
     ts->debug_print_nodes();
-    delete ts;
 
 //    linear_regression lr(1);
 //    std::vector< std::vector<float> > x {
